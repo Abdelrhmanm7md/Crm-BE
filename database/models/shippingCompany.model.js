2854573cd7c6905630da02c92935ed3d30a36478
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2025-03-28 01:13:17 +0200

    /

--- database/models/shippingCompany.model.js
+++ database/models/shippingCompany.model.js
@@ -188,7 +188,7 @@ shippingCompanySchema.post("find", async function (docs) {
           $sum: {
             $cond: [
               { $in: ["$orderStatus", ["shipping", "completed"]] },
-              { $subtract: ["$realTotalAmount", "$realShippingPrice"] }, // Subtract shippingPrice
+              { $subtract: ["$realTotalAmount", "$realShippingPrice"] }, 
               0
             ]
           }
@@ -198,17 +198,22 @@ shippingCompanySchema.post("find", async function (docs) {
             $cond: [{ $eq: ["$orderStatus", "shipping"] }, "$_id", "$$REMOVE"] 
           }
         }
-      },
+      }
+    },
+    {
+      $lookup: {
+        from: "orders",  // Ensure this is the actual MongoDB collection name
+        localField: "orders",
+        foreignField: "_id",
+        as: "orders"
+      }
     }
-  ]);  
+  ]); 
 
   const statsMap = new Map();
   orderStats.forEach((stat) => {
     statsMap.set(stat._id.toString(), stat);
   });
-  for (let stat of orderStats) {
-    stat.orders = await orderModel.find({ _id: { $in: stat.orders } });
-  }
 docs.forEach((doc) => {
   const stat = statsMap.get(doc._id.toString());
   doc.ordersCount = stat?.shippingOrders || 0;
